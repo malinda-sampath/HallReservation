2fd2224a0ee35074c558946e7d1c249f44c64b8f
malinda-sampath <malindasampath45@gmail.com>
2024-08-14 16:43:52 +0530

    Essential files added

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\Auth\LoginController;
+    use App\Http\Controllers\DataEntryController;
+
+    Route::get('/contact', function () {
+        return view('contact');
+    });
+
+    Route::get('/index', function () {
+        return view('index');
+    });
+
+    Route::get('/index', function () {
+        return view('index');
+    })->name('index');
+
+    Auth::routes();
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Authentication Routes
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // Admin Routes
+    Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'AdminController@index')->name('home');
+    });
+
+    Auth::routes();
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/home', [DataEntryController::class, 'store']);
+    
+
+    
